192828b383d95c6675394a2f71dbd3d23c112633
billel <bilalbxw0@gmail.com>
2022-04-02 12:55:08 -0700

    Fix Error

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,3 +3,28 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+
+const About = ({ data, location }) => {
+    const siteTitle = data.site.siteMetadata.title
+  
+    return (
+      <Layout location={location} title={siteTitle}>
+        <Seo title="About" />
+        <h1>Welcome To About Page</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Layout>
+    )
+  }
+  
+  export default About
+  
+  export const pageQuery = graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `
+  
